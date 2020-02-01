50510bf10c5a4aa03ff0f7eacdb594242148656c
oscar02r <oscary.ramirez@gmail.com>
2020-02-01 23:05:06 +0100

    Added of get user- movie with query

--- index.js
+++ index.js
@@ -57,9 +57,26 @@ app.post('/auth/sign-up', async function(req, res, next){
     }
 });
 
+app.post('/movie-create', async function(req, res, next){
+
+         const {body:movie} = req;
+         const {token} = req.cookies;
+     try {
+       await axios({
+         url:`${config.apiUrl}/api/movies/create`,
+         method:'post',
+         data: movie,
+         headers:{Authorization:`Bearer ${token}`},
+       });
+       res.status(200).json({message:'Movie created'})
+     } catch (error) {
+       next(error);
+     }
+});
+
 app.get('/movies', async function(req, res, next){
   
-        const {token} = req.cookies;
+        const { token } = req.cookies;
         const {data , status} = await axios({
           url:`${config.apiUrl}/api/movies`,
           method:'get',
@@ -73,11 +90,54 @@ app.get('/movies', async function(req, res, next){
         res.status(200).json(data);
 });
 
+app.get('/movie/:movieId', async function(req, res, next){
+  try {
+    
+    const {movieId} = req.params;
+    const {token} = req.cookies;
+    const {data , status} = await axios({
+      url:`${config.apiUrl}/api/movies/${movieId}`,
+      method:'get',
+      headers:{Authorization:`Bearer ${token}`},
+    });
+    if(!data || status !==200 ){
+       next(boom.badImplementation('Bad implemation'));
+    }
+    
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+
+});
+
+app.get("/user-movies", async function(req, res, next) {
+  try {
+    
+    const { token } = req.cookies;
+    const {id} = req.query;
+
+    const { data, status } = await axios({
+      url: `${config.apiUrl}/api/user-movies?id=${id}`,
+      headers: { Authorization: `Bearer ${token}` },
+      method: "get"
+    });
+
+    if (status !== 200) {
+      return next(boom.badImplementation());
+    }
+  
+    res.status(200).json(data);
+  } catch (error) {
+    next(error);
+  }
+});
+
 app.post("/user-movies", async function(req, res, next) {
   try {
     const { body: userMovie } = req;
     const { token } = req.cookies;
-
+   
     const { data, status } = await axios({
       url: `${config.apiUrl}/api/user-movies`,
       headers: { Authorization: `Bearer ${token}` },
@@ -88,12 +148,36 @@ app.post("/user-movies", async function(req, res, next) {
     if (status !== 201) {
       return next(boom.badImplementation());
     }
-
+  
     res.status(201).json(data);
   } catch (error) {
     next(error);
   }
 });
+
+app.delete('/movie-delete/:id', async function(req, res, next){
+          
+
+          try {
+            const { id } = req.params;
+            const {token} =  req.cookies;
+            const {data, status} =   await axios({
+                   url:`${config.apiUrl}/api/movies/${id}`,
+                   headers:{Authorization:`Bearer ${token}`},
+                   method:'delete'
+               });
+     
+               if(status !== 200){
+                return next(boom.badImplementation());
+                }
+    
+
+               res.status(200).json({data})
+          } catch (error) {
+             next(error);
+          }
+});
+
 
 app.delete('/user-movies/:userMovieId', async function(req, res, next){
     try{
